88530d0184c832af5e1dbbe254c6270dc224e35d
ttkhlashidze <ttkhlashidze@bog.ge>
2016-09-01 18:38:42 +0400

    init commit

---
+++ VotingApp/app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import {Component} from "@angular/core";
+
+
+@Component({
+    selector: "voting-app",
+    template: `  this is voting app `
+})
+
+export class AppComponent {
+
+}
---
+++ VotingApp/app/app.routes.ts
@@ -0,0 +1,8 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+export const CustomRoutes: Routes = [
+    //{ path: '', component: '' },
+
+];
+
+export const CustomRouting = RouterModule.forRoot(CustomRoutes);
