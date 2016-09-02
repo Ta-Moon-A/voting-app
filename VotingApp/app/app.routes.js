54e377056098fb4680309b551eae3b7529b73397
ttkhlashidze <ttkhlashidze@bog.ge>
2016-09-02 11:53:42 +0400

    commit staged

--- VotingApp/app/app.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var router_1 = require('@angular/router');
-exports.CustomRoutes = [
-    { path: '', component: 'MainBoardComponent' }
-];
-exports.CustomRouting = router_1.RouterModule.forRoot(exports.CustomRoutes);
-//# sourceMappingURL=app.routes.js.map
--- VotingApp/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Routes, RouterModule } from '@angular/router';
-import {MainBoardComponent} from './votingApp/main.board.component';
-
-export const CustomRoutes: Routes = [
-    { path: '', component: 'MainBoardComponent' }
-
-];
-
-export const CustomRouting = RouterModule.forRoot(CustomRoutes);
