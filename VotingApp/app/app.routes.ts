3176c7f5d4be2e640bf754b54fef920471cc89a1
ttkhlashidze <ttkhlashidze@bog.ge>
2016-09-02 11:56:42 +0400

    change 3

---
+++ VotingApp/app/app.routes.ts
@@ -0,0 +1,9 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+import {MainBoardComponent} from './votingApp/main.board.component';
+
+export const CustomRoutes: Routes = [
+    { path: '', component: 'MainBoardComponent' }
+
+];
+
+export const CustomRouting = RouterModule.forRoot(CustomRoutes);
